e0a294a3a6c666b3f97c58bfaacace9db1876757
Dave Kelly <dave.kelly@avire-global.com>
2018-02-20 15:30:23 +0000

    Initial Version

---
+++ extension.js
@@ -0,0 +1,35 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "ccs-builder" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.sendBuildCommand', function () {
+        // The code you place here will be executed every time your command is executed
+
+        var tmp = require('tmp');
+
+        tmp.file({ prefix: 'Start', postfix: '.txt' }, function _tempFileCreated(err, path, fd) {
+            if (err) throw err;
+            vscode.window.showInformationMessage('Created temporary filename: ', path);
+        });
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
